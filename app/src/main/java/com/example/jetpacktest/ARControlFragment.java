c6afc10bbc884d3fd7f6e72054723f0afe1a30d7
Chan Woo <14cpark@gmail.com>
2019-12-01 13:05:47 -0800

    Create ARControlFragment with ViewModel

---
+++ app/src/main/java/com/example/jetpacktest/ARControlFragment.java
@@ -0,0 +1,36 @@
+package com.example.jetpacktest;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ARControlFragment extends Fragment {
+
+	private ARControlViewModel mViewModel;
+
+	public static ARControlFragment newInstance() {
+		return new ARControlFragment();
+	}
+
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+	                         @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_arcontrol, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		mViewModel = ViewModelProviders.of(this).get(ARControlViewModel.class);
+		// TODO: Use the ViewModel
+	}
+
+}
---
+++ app/src/main/java/com/example/jetpacktest/ARControlViewModel.java
@@ -0,0 +1,7 @@
+package com.example.jetpacktest;
+
+import androidx.lifecycle.ViewModel;
+
+public class ARControlViewModel extends ViewModel {
+	// TODO: Implement the ViewModel
+}
