61cd9fe79c41a9ff3c23ad229cdb495d9831a67e
chanwoo <14cpark@gmail.com>
2019-12-02 00:37:58 -0800

    Move Text & Button widgets from Activity to ARControlFragment

--- app/src/main/java/com/example/jetpacktest/ARControlFragment.java
+++ app/src/main/java/com/example/jetpacktest/ARControlFragment.java
@@ -1,5 +1,7 @@
 package com.example.jetpacktest;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ARControlFragment extends Fragment {
 
@@ -30,7 +34,30 @@ public class ARControlFragment extends Fragment {
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		mViewModel = ViewModelProviders.of(getActivity()).get(MainActivityViewModel.class);
-		// TODO: Use the ViewModel
+
+		LiveData<String> myRandomNumber = mViewModel.getMldNumber();
+
+		TextView tvNumber = getView().findViewById(R.id.tvNumber);
+		Button bCreateNumber = getView().findViewById(R.id.bCreateNumber);
+
+		/*
+			First param in .observe(): use getViewLifecycleOwner() instead of 'this'
+			LiveData obj will be scoped to the lifecycle of the fragment's viewer, instead of the fragment instance
+			Which means when the viewer is destroyed, the observer gets removed regardless of whether the fragment instance is still alive.
+		*/
+		myRandomNumber.observe(getViewLifecycleOwner(), new Observer<String>() {
+			@Override
+			public void onChanged(String s) {
+				tvNumber.setText(s);
+			}
+		});
+
+		bCreateNumber.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mViewModel.createNumber();
+			}
+		});
 	}
 
 }
--- app/src/main/java/com/example/jetpacktest/MainActivity.java
+++ app/src/main/java/com/example/jetpacktest/MainActivity.java
@@ -17,26 +17,12 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final TextView tvNumber = findViewById(R.id.tvNumber);
-		Button bCreateNumber = findViewById(R.id.bCreateNumber);
+
 
 		getLifecycle().addObserver(new MainActivityObserver());
 
 		final MainActivityViewModel viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
-		final LiveData<String> myRandomNumber = viewModel.getMldNumber();
 
-		myRandomNumber.observe(this, new Observer<String>() {
-			@Override
-			public void onChanged(String s) {
-				tvNumber.setText(s);
-			}
-		});
 
-		bCreateNumber.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				viewModel.createNumber();
-			}
-		});
 	}
 }
